51469d509ff01476e6977f8105e846b709c52e8f
Praphul04 <praphulthakur6@gmail.com>
2023-08-10 10:06:49 +0530

    first commit

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,52 @@
 import Card from '../UI/Card';
-import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem';
-
+import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Chicken Biryani',
-        description: 'Finest checken and Curry',
-        price: 150,
-    },
-    {
-        id: 'm2',
-        name: 'Chicken Corma',
-        description: 'A Indian specialty!',
-        price: 250,
-    },
-    {
-        id: 'm3',
-        name: 'Chicken Burger',
-        description: 'Bread, chicken Keema And vaggies',
-        price: 100,
-    },
-    {
-        id: 'm4',
-        name: 'Chicken Soup',
-        description: 'Healthy...and delecious...',
-        price: 50,
-    },
+  {
+    id: 'm1',
+    name: 'Chicken Biryani',
+    description: 'Finest chicken and Basmati rice',
+    price: 150,
+  },
+  {
+    id: 'm2',
+    name: 'Chicken Curry',
+    description: 'A Indian specialty!',
+    price: 200,
+  },
+  {
+    id: 'm3',
+    name: 'Chicken Burger',
+    description: 'Indian, raw, yammy',
+    price: 100,
+  },
+  {
+    id: 'm4',
+    name: 'Chicken Soup',
+    description: 'Healthy...and Testy...',
+    price: 130,
+  },
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) =>
-    <MealItem 
-    key={meal.id} 
-    name={meal.name} 
-    description={meal.description} 
-    price={meal.price}
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
     />
-    );
+  ));
 
-    return (
-        <section className={classes.meals}>
-            <Card>
-                <ul>{mealsList}</ul>
-            </Card>
-        </section>
-    );
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}  onClick={props.onClose} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
   return (
-    <div className={classes.modal} >
+    <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
     </div>
   );
@@ -20,7 +20,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClick={props.onClose}/>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
